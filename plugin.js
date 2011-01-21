843973cee58632b75ebfbba84800199ba7e936b3
Andrey Sidorov <takandar@gmail.com>
2011-01-21 14:46:29 +0300

    updates

--- plugin.js
+++ plugin.js
@@ -18,32 +18,32 @@ CKEDITOR.htmlDataProcessor.prototype =
   toHtml : function( data, fixForBody )
   {
     // Convert < and > to their HTML entities.
-    data = data.replace( /</g, '&lt;' ) ;
-    data = data.replace( />/g, '&gt;' ) ;
+    data = data.replace( /</g, '&lt;' );
+    data = data.replace( />/g, '&gt;' );
 
     // Convert line breaks to <br>.
-    data = data.replace( /(?:\r\n|\n|\r)/g, '<br>' ) ;
+    data = data.replace( /(?:\r\n|\n|\r)/g, '<br>' );
 
     // [url]
-    data = data.replace( /\[url\](.+?)\[\/url]/gi, '<a href="$1">$1</a>' ) ;
-    data = data.replace( /\[url\=([^\]]+)](.+?)\[\/url]/gi, '<a href="$1">$2</a>' ) ;
+    data = data.replace( /\[url\](.+?)\[\/url]/gi, '<a href="$1">$1</a>' );
+    data = data.replace( /\[url\=([^\]]+)](.+?)\[\/url]/gi, '<a href="$1">$2</a>' );
 
     // [b]
-    data = data.replace( /\[b\](.*?)\[\/b]/gi, '<b>$1</b>' ) ;
+    data = data.replace( /\[b\](.*?)\[\/b]/gi, '<b>$1</b>' );
 
     // [i]
-    data = data.replace( /\[i\](.*?)\[\/i]/gi, '<i>$1</i>' ) ;
+    data = data.replace( /\[i\](.*?)\[\/i]/gi, '<i>$1</i>' );
 
     // [u]
-    data = data.replace( /\[u\](.*?)\[\/u]/gi, '<u>$1</u>' ) ;
+    data = data.replace( /\[u\](.*?)\[\/u]/gi, '<u>$1</u>' );
 
     // [img]
     data = data.replace(/\[img\](.*?)\[\/img\]/gi,'<img src="$1" />');
 
     // [quote]
-    //data = data.replace( /\[quote\](.+?)\[\/quote]/gi, '<quote>$1</quote>' ) ;
-    data = data.replace( /\[quote\]/gi, '<blockquote>' ) ;
-    data = data.replace( /\[\/quote]/gi, '</blockquote>' ) ;
+    //data = data.replace( /\[quote\](.+?)\[\/quote]/gi, '<quote>$1</quote>' );
+    data = data.replace( /\[quote\]/gi, '<blockquote>' );
+    data = data.replace( /\[\/quote]/gi, '</blockquote>' );
 
     // [code]
     data = data.replace(/\[code\]/gi,'<code>');
@@ -70,60 +70,60 @@ CKEDITOR.htmlDataProcessor.prototype =
     }
     // Convert <br> to line breaks.
     html = html.replace(/<br><\/p>/gi,"\n");
-    html = html.replace( /<br(?=[ \/>]).*?>/gi, '\r\n') ;
+    html = html.replace(/<br(?=[ \/>]).*?>/gi, '\r\n');
     html = html.replace(/<p>/gi,"");
     html = html.replace(/<\/p>/gi,"\n");
     html = html.replace(/&nbsp;/gi," ");
 
     // [url]
-    html = html.replace( /<a .*?href=(["'])(.+?)\1.*?>(.+?)<\/a>/gi, '[url=$2]$3[/url]') ;
+    html = html.replace( /<a .*?href=(["'])(.+?)\1.*?>(.+?)<\/a>/gi, '[url=$2]$3[/url]');
     //html = html.replace(/<a.*?href=\"(.*?)\".*?>(.*?)<\/a>/gi,"[url=$1]$2[/url]");
 
     // [b]
-    html = html.replace( /<(?:b|strong)>/gi, '[b]') ;
-    html = html.replace( /<\/(?:b|strong)>/gi, '[/b]') ;
+    html = html.replace( /<(?:b|strong)>/gi, '[b]');
+    html = html.replace( /<\/(?:b|strong)>/gi, '[/b]');
 
     // [i]
-    html = html.replace( /<(?:i|em)>/gi, '[i]') ;
-    html = html.replace( /<\/(?:i|em)>/gi, '[/i]') ;
+    html = html.replace( /<(?:i|em)>/gi, '[i]');
+    html = html.replace( /<\/(?:i|em)>/gi, '[/i]');
 
     // [u]
-    html = html.replace( /<u>/gi, '[u]') ;
-    html = html.replace( /<\/u>/gi, '[/u]') ;
+    html = html.replace( /<u>/gi, '[u]');
+    html = html.replace( /<\/u>/gi, '[/u]');
 
     // smileys
     html = html.replace( /<img .*?src=(["']).+?(:.+?:?).gif\1.*?>/gi, '$2');
 
     // [img]
     //html = html.replace(/<img.*?src=\"(.*?)\".*?\/>/gi,"[img]$1[/img]");
-    //html = html.replace( /<img .*?src=(["'])(.+?)\1.*?\/>/gi, '[img]$2[/img]') ;
-    html = html.replace( /<img .*?src=(["'])(.+?)\1.*?>/gi, '[img]$2[/img]') ;
+    //html = html.replace( /<img .*?src=(["'])(.+?)\1.*?\/>/gi, '[img]$2[/img]');
+    html = html.replace( /<img .*?src=(["'])(.+?)\1.*?>/gi, '[img]$2[/img]');
 
     // [quote]
-    html = html.replace( /<blockquote>/gi, '[quote]') ;
-    html = html.replace( /\n*<\/blockquote>/gi, '[/quote]') ;
+    html = html.replace( /<blockquote>/gi, '[quote]');
+    html = html.replace( /\n*<\/blockquote>/gi, '[/quote]');
 
     // [code]
-    html = html.replace( /<code>/gi, '[code]') ;
-    html = html.replace( /<\/code>/gi, '[/code]') ;
+    html = html.replace( /<code>/gi, '[code]');
+    html = html.replace( /<\/code>/gi, '[/code]');
 
     // [color]
     html = html.replace(/<span style=\"color: ?(.*?);\">(.*?)<\/span>/gi,"[color=$1]$2[/color]");
     html = html.replace(/<font.*?color=\"(.*?)\".*?>(.*?)<\/font>/gi,"[color=$1]$2[/color]");
 
     // Remove remaining tags.
-    html = html.replace( /<[^>]+>/g, '') ;
+    html = html.replace( /<[^>]+>/g, '');
 
     // Restore < and >
-    html = html.replace( /&lt;/g, '<' ) ;
-    html = html.replace( /&gt;/g, '>' ) ;
+    html = html.replace( /&lt;/g, '<' );
+    html = html.replace( /&gt;/g, '>' );
 
     // Restore ( and )
-    html = html.replace( /%28/g, '(' ) ;
-    html = html.replace( /%29/g, ')' ) ;
+    html = html.replace( /%28/g, '(' );
+    html = html.replace( /%29/g, ')' );
 
     // Restore %20
-    html = html.replace( /%20/g, ' ' ) ;
+    html = html.replace( /%20/g, ' ' );
 
 
     return html;
